2b7dae07d8cccd219515acae7215c863c3d9a04a
Robin Mattheussen <me@romatthe.dev>
2022-02-16 21:16:54 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "An implmentation of Robert Nystrom's Lox language as seen in `Crafting Interpreters`, in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-version = "1.58.1";
+        rust-stable = pkgs.rust-bin.stable.${rust-version}.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+        {
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rust-stable
+              # We want this one for ca65, an assembler for 6502 assembly
+              cc65
+            ];
+          };
+        }
+    );
+}
